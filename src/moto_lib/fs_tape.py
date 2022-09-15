da3b44a81a5876a07bd5d040be277140df351de8
David SPORN <sporniket.studio@gmail.com>
2022-09-15 07:04:21 +0200

    draft some code and tests

---
+++ src/moto_lib/fs_tape.py
@@ -0,0 +1,83 @@
+"""
+File system on tape.
+---
+(c) 2022 David SPORN
+---
+This is part of MO/TO tools.
+
+MO/TO tools is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your option)
+any later version.
+
+MO/TO tools is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with MO/TO tools.
+If not, see <https://www.gnu.org/licenses/>. 
+---
+"""
+
+from enum import Enum, IntEnum
+
+
+class TypeOfTapeBloc(IntEnum):
+    LEADER = 0
+    DATA = 1
+    EOF = 2
+
+
+class TapeBloc:
+    def __init__(self, rawData, readOnly=True):
+        self.rawData = rawData
+        self.readOnly = readOnly
+
+    @property
+    def type(self):
+        return TypeOfTapeBloc(self.rawData[0])
+
+    @property
+    def length(self):
+        return self.rawData[1]
+
+    @property
+    def checksum(self):
+        return self.rawData[-1]
+
+    @property
+    def body(self):
+        return self.rawData[2:-1]  # FIXME
+
+    def isValid(self):
+        pass
+
+
+class LeaderTapeBlocDescriptor:
+    def __init__(self, fileName, fileExtension, fileType, fileMode):
+        self.fileName = fileName  # TODO decode + trim, to upper
+        self.fileExtension = fileExtension  # TODO decode + trim, to upper
+        self.fileType = fileType  # Restrict to 0..255
+        self.fileMode = fileMode  # Restrict to 0..65535
+
+    @staticmethod
+    def buildFromTapeBloc(rawData):
+        return LeaderTapeBlocDescriptor(
+            rawData[2:10], rawData[10:13], rawData[13], rawData[14] * 256 + rawData[15]
+        )
+
+    def toTapeBloc(self) -> TapeBloc:
+        return TapeBloc(rawData)
+
+
+class Tape:
+    @property
+    def position(self):
+        return self.position
+
+    def rewind(self):
+        self.position = 0
+
+    def nextBloc(self) -> TapeBloc:
+        pass
---
+++ tests/units/test_fs_tape.py
@@ -0,0 +1,76 @@
+"""
+---
+(c) 2022 David SPORN
+---
+This is part of MO/TO tools.
+
+MO/TO tools is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your option)
+any later version.
+
+MO/TO tools is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with MO/TO tools.
+If not, see <https://www.gnu.org/licenses/>. 
+---
+"""
+from moto_lib.fs_tape import TapeBloc
+import pytest
+
+
+def test_tape_bloc_with_valid_checksum_should_be_valid():
+    but = TapeBloc(
+        bytes(
+            [
+                0x00,
+                0x10,
+                0x42,
+                0x41,
+                0x4E,
+                0x4E,
+                0x45,
+                0x52,
+                0x20,
+                0x20,
+                0x42,
+                0x41,
+                0x53,
+                0x00,
+                0x00,
+                0x00,
+                0x34,
+            ]
+        )
+    )
+    assert but.isValid() == True
+
+
+def test_tape_bloc_with_invalid_checksum_should_be_invalid():
+    but = TapeBloc(
+        bytes(
+            [
+                0x00,
+                0x10,
+                0x42,
+                0x41,
+                0x4E,
+                0x4E,
+                0x45,
+                0x52,
+                0x20,
+                0x20,
+                0x42,
+                0x41,
+                0x53,
+                0x00,
+                0x00,
+                0x00,
+                0x33,
+            ]
+        )
+    )
+    assert but.isValid() == False
