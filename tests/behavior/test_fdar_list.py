5ee898a13bc0daafabb212597286520914a7045e
David SPORN <sporniket.studio@gmail.com>
2023-12-23 08:56:17 +0100

    change test to simple raise to have report

--- tests/behavior/test_fdar_list.py
+++ tests/behavior/test_fdar_list.py
@@ -28,49 +28,19 @@ from typing import List, Union, Optional
 from unittest.mock import patch
 from contextlib import redirect_stdout
 
-from moto_fdar import DiskArchiveCli
+# from moto_fdar import DiskArchiveCli
 
 from .utils import (
     makeTmpDirOrDie,
     assert_that_source_is_converted_as_expected,
 )
 
-input_archive = "sporny-basic.k7"
+# input_archive = "sporny-basic.k7"
 
 
 def test_that_it_does_list_files():
-    source_dir = os.path.join(".", "tests", "data")
-    baseArgs = ["prog", "-t", os.path.join(source_dir, input_archive)]
-    with patch.object(sys, "argv", baseArgs):
-        with redirect_stdout(io.StringIO()) as out:
-            returnCode = DiskArchiveCli().run()
-        assert returnCode == 0
-        assert (
-            out.getvalue()
-            == """BANNER.BAS
-BANNER2.BAS
-C5000.BAS
-C5001.BAS
-C5001LST.BAS
-C5002.BAS
-"""
-        )
+    raise RuntimeError("Not implemented")
 
 
 def test_that_verbose_mode_does_list_files_with_details():
-    source_dir = os.path.join(".", "tests", "data")
-    baseArgs = ["prog", "-tv", os.path.join(source_dir, input_archive)]
-    with patch.object(sys, "argv", baseArgs):
-        with redirect_stdout(io.StringIO()) as out:
-            returnCode = DiskArchiveCli().run()
-        assert returnCode == 0
-        assert (
-            out.getvalue()
-            == """BANNER.BAS\tBASIC\tTOKEN\t#1\t102 octets\t1 blocks.
-BANNER2.BAS\tBASIC\tTOKEN\t#4\t102 octets\t1 blocks.
-C5000.BAS\tBASIC\tTOKEN\t#7\t794 octets\t4 blocks.
-C5001.BAS\tBASIC\tTOKEN\t#13\t804 octets\t4 blocks.
-C5001LST.BAS\tBASIC\tASCII\t#19\t942 octets\t4 blocks.
-C5002.BAS\tBASIC\tTOKEN\t#25\t836 octets\t4 blocks.
-"""
-        )
+    raise RuntimeError("Not implemented")
